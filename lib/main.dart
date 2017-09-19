c0494da86ea8be0fc3fde3aa75a01676ddd45173
sudakov <vsudakov@bk.ru>
2017-09-19 22:25:07 +0300

    ConfigScreen

--- lib/main.dart
+++ lib/main.dart
@@ -5,59 +5,25 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Экспедитор',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Маршруты и инкассации'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     
@@ -69,64 +35,128 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               new BottomNavigationBarItem(         
                     icon: const Icon(Icons.account_box),
-                    title: const Text('Учетная запись'),
-                    backgroundColor: Colors.deepOrange,
+                    title: const Text('Настройки'),
+                    backgroundColor: Theme.of(context).primaryColor,
               ),
       ],
-      currentIndex: 0,
+      currentIndex: _currentIndex,
       type: BottomNavigationBarType.shifting,
-      //fixedColor: Theme.of(context).primaryColor,
+      onTap: (int index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
     );
-    
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return new Scaffold(
+      body: new Stack(        
+          children: <Widget>[
+            new Offstage(
+              offstage: _currentIndex != 0,
+              child: new TickerMode(
+                enabled: _currentIndex == 0,
+                child: new MainScreen(),
+              ),
+            ),
+            new Offstage(
+              offstage: _currentIndex != 1,
+              child: new TickerMode(
+                enabled: _currentIndex == 1,
+                child: new ConfigScreen(),
+              ),
+            ),          
+          ],
+      ),
+      bottomNavigationBar: botNavBar,
+    );
+  }
+}
+
+
+typedef void CfgChangedCallback();
+  
+class ConfigScreen extends StatefulWidget {
+//  MyConfig cfg;
+  final CfgChangedCallback onCfgChanged;
+//  ConfigScreen({Key key, this.cfg, this.onCfgChanged}) : super(key: key);
+
+  @override                                                         
+  State createState() => new ConfigScreenState();                    
+}
+
+class ConfigScreenState extends State<ConfigScreen> {
+  
+  final TextEditingController _controller = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+//  _controller.text = widget.cfg.apiCode;
+//    _controller.addListener(() {
+//      widget.cfg.apiCode = _controller.text;
+//      widget.onCfgChanged();
+//    });
+  }
+  
+  @override                                                         
+  Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text("Настройки подключения")
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: new Container(
+        padding: const EdgeInsets.all(32.0),
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            new Text('Телефон или e-mail или имя'),
+            new TextField(
+              decoration: new InputDecoration(
+                hintText: 'Введите телефон или e-mail или имя',
+              ),
             ),
-            new Text(
-              '${_counter}',
-              style: Theme.of(context).textTheme.display1,
+            new Text('Пароль'),
+            new TextField(
+              controller: _controller,
+              decoration: new InputDecoration(
+                hintText: 'Введите пароль',
+              ),
             ),
           ],
         ),
       ),
-      
-      bottomNavigationBar: botNavBar,
-      
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+
+class MainScreen extends StatefulWidget {
+  @override                                                         
+  State createState() => new MainScreenState();                    
+}
+
+class MainScreenState extends State<MainScreen> {
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  
+  @override                                                         
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Маршруты и инкассации")
+      ),
+      body: new Container(
+        padding: const EdgeInsets.all(32.0),
+        child: new Column(
+          children: <Widget>[
+            new Text('Hello world!!!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
