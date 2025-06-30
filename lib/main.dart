8030df8c1a9906a60c0c3fd2cfa06caccaa9c0f0
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2025-06-30 09:58:56 +0300

    DEV-43594

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,12 @@ void main() async {
           RepositoryProvider.value(value: usersRepository)
         ],
         child: MaterialApp(
+          builder: (context, child) {
+            return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(1.0)),
+                child: child!,
+              );
+          },
           scaffoldMessengerKey: scaffoldMessengerKey,
           title: Strings.ruAppName,
           theme: FlexThemeData.light(
